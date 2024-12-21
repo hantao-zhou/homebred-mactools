1516cdb2267d813e0a100748ddbe534787b4f8b2
Hans Gutmann <hansgutmann@172-15-71-41.lightspeed.austtx.sbcglobal.net>
2024-12-21 14:49:41 +0800

    Add replace formula

---
+++ Formula/replace.rb
@@ -0,0 +1,22 @@
+class Replace < Formula
+    include Language::Python::Virtualenv
+  
+    desc "A Python script to replace text in files"
+    homepage "https://github.com/hantao-zhou/replace"
+    url "https://github.com/hantao-zhou/replace/archive/refs/tags/v0.1.tar.gz"
+    sha256 "7321ba9f6b0484e38d4d6e8e07d7423233cdb16546338a6f69f98c134eac0d4c"
+    license "MIT"
+  
+    depends_on "python@3.9"
+  
+    def install
+      bin.install "bin/replace.py"
+      # Make the script executable
+      bin.install_symlink "replace.py" => "replace"
+    end
+  
+    test do
+      system "#{bin}/replace", "--version"
+    end
+  end
+  
